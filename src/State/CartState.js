316e4d58b14d7e15942593017d562fc005b34d07
Vinh Nguyen Trung <vinh.nguyen@kyanon.digital>
2022-05-08 13:19:47 +0700

    update

--- src/State/CartState.js
+++ src/State/CartState.js
@@ -1,69 +1,86 @@
-import { atom, selector } from 'recoil';
+import { atom, selector } from "recoil";
 
-const localStorageEffect = key => ({ setSelf, onSet }) => {
-    const savedValue = localStorage.getItem(key)
-    if (savedValue != null) {
+const localStorageEffect =
+  (key) =>
+    ({ setSelf, onSet }) => {
+      const savedValue = localStorage.getItem(key);
+      if (savedValue != null) {
         setSelf(JSON.parse(savedValue));
-    }
+      }
 
-    onSet((newValue, _, isReset) => {
+      onSet((newValue, _, isReset) => {
         isReset
-            ? localStorage.removeItem(key)
-            : localStorage.setItem(key, JSON.stringify(newValue));
-    });
-};
+          ? localStorage.removeItem(key)
+          : localStorage.setItem(key, JSON.stringify(newValue));
+      });
+    };
 
 export const cartState = atom({
-    key: 'cart',
-    default: [],
-    effects: [
-        localStorageEffect('cart'),
-    ]
+  key: "cart",
+  default: [],
+  effects: [localStorageEffect("cart")],
 });
 
 export const addToCart = (cart, item) => {
-    const newCart = [...cart];
-    const foundIndex = cart.findIndex(x => x.id === item._id);
-    if (foundIndex >= 0) {
-        newCart[foundIndex] = {
-            ...cart[foundIndex],
-            quantity: cart[foundIndex].quantity + 1,
-        };
-        return newCart;
-    }
-    newCart.push({
-        id: item._id,
-        product: item,
-        quantity: 1,
-    });
+  const newCart = [...cart];
+  const foundIndex = cart.findIndex((x) => x.id === item._id);
+  if (foundIndex >= 0) {
+    newCart[foundIndex] = {
+      ...cart[foundIndex],
+      quantity: cart[foundIndex].quantity + 1,
+    };
     return newCart;
+  }
+  newCart.push({
+    id: item._id,
+    product: item,
+    quantity: 1,
+  });
+  return newCart;
 };
 
-export const deleteItemCart = (cart, idDelete   ) => {
-    const newCart = [...cart];
-    return newCart.filter((item) => {
-        return item.id !== idDelete
-    })
+export const deleteItemCart = (cart, idDelete) => {
+  const newCart = [...cart];
+  return newCart.filter((item) => {
+    return item.id !== idDelete;
+  });
 };
 
+export const decreaseQuantity = (cart, product) => {
+  const newCart = [...cart];
+  //   check  product available in cart
+  const foundIndex = cart.findIndex((x) => x.id === product._id);
+  if (foundIndex >= 0) {
+   if(cart[foundIndex].quantity > 0){
+    newCart[foundIndex] = {
+      ...cart[foundIndex],
+      quantity: cart[foundIndex].quantity - 1,
+    };
+   }
+   else{
+     console.log('mat spham luon');
+     console.log(product);
+   }
+    return newCart;
+  }
+};
 
 export const cartQuantity = selector({
-    key: 'cartQuantity',
-    get: ({ get }) => {
-        const cart = get(cartState);
-        return cart.reduce((total, item) => {
-            return total + (item?.quantity);
-        }, 0);
-    }
+  key: "cartQuantity",
+  get: ({ get }) => {
+    const cart = get(cartState);
+    return cart.reduce((total, item) => {
+       return total + item?.quantity;
+    }, 0);
+  },
 });
 
-
 export const cartTotal = selector({
-    key: 'cartTotal',
-    get: ({ get }) => {
-        const cart = get(cartState);
-        return cart.reduce((total, item) => {
-            return total + (item?.product?.price * item?.quantity);
-        }, 0);
-    }
+  key: "cartTotal",
+  get: ({ get }) => {
+    const cart = get(cartState);
+    return cart.reduce((total, item) => {
+        return total + item?.product?.price * item?.quantity;
+    }, 0);
+  },
 });
